308cca8c79359ec7d5604e0135a613c5644f8db8
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2023-09-22 17:19:56 -0500

    Ajustes 5

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -276,7 +276,7 @@
     )
       figure.col-xl-10.mx-auto(data-aos="fade-left")
         .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/e2UrrgA0pDA?si=ghwYMpUr55dCGM-v" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     Separador 
 
